e000abde5a60cedbaba44adea2310590610f35c3
mweb-3 <cgamerdoggo@gmail.com>
2026-03-17 22:28:43 -0500

    redesign entire thing

--- api/index.py
+++ api/index.py
@@ -1,36 +1,12 @@
 from flask import Flask, render_template, send_from_directory
 import datetime
-import json
-from urllib.parse import unquote
-
-
-def load_songs():
-    with open("api/songs.json", "r", encoding="utf-8") as playlist:
-        songs = json.load(playlist)
-    return songs
-
-
-songs = load_songs()
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
-    date = int(datetime.datetime.now().strftime("%d")) - 1
-
-    sootd = songs["songs"][date]
-    return render_template("index.html", sootd=sootd)
-
-
-@app.route("/songs/<path:filename>")
-def serve_audio(filename):
-    return send_from_directory("songs", unquote(filename))
-
-
-@app.route("/songs.json")
-def songsList():
-    return send_from_directory("", "songs.json")
+@app.route('/')
+def new():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
